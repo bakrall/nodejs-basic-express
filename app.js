eda3acff787952ffacaed74b362592fc667615bb
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-06-03 07:34:26 +0200

    set up initial app file

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require ("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hi there! Welcome to my assignement."));
+
+app.listen(port, () => console.log("Server has started"));
