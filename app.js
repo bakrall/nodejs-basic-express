1b3b5b882ea70161af0a2d2a962877a2c002219c
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-06-04 21:35:28 +0200

    refactor to be ES6

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hi there! Welcome to my assignement."));
 
-app.get("/speak/:animal", function(req, res) {
+app.get("/speak/:animal", (req, res) => {
     if (req.params.animal === 'pig') {
         res.send("The pig says Oink");
     } else if (req.params.animal === 'cow') {
@@ -14,21 +14,19 @@ app.get("/speak/:animal", function(req, res) {
     }
 });
 
-app.get("/repeat/:word/:times", function (req, res) {
-    var times = req.params.times;
-    var word = req.params.word;
-    var sentence = ""
+app.get("/repeat/:word/:times", (req, res) => {
+    const times = req.params.times;
+    const word = req.params.word;
+    let sentence = ""
     
-    for (var i = 0; i < times; i++) {
+    for (let i = 0; i < times; i++) {
         sentence += word + ' ';
     }
     
     res.send(sentence);
 });
 
-app.get("*", function(req, res) {
-    res.send("Sorry, page not found");
-});
+app.get("*", (req, res) => res.send("Sorry, page not found"));
 
 
 app.listen(port, () => console.log("Server has started"));
