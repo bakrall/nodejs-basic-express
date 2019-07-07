d52e73d0ad9b7e4e2ddaf9579560856d1af1be7a
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-07-07 18:49:27 +0200

    EJS basic loop example

--- EJSExample/app.js
+++ EJSExample/app.js
@@ -9,7 +9,17 @@ app.get("/", (req, res) => {
 app.get("/subpage/:thing", (req, res) => {
 	let thing = req.params.thing;
 
-	res.render("subpage.ejs", {thingVar: thing})
+	res.render("subpage.ejs", {thingVar: thing});
+});
+
+app.get("/posts", (req, res) => {
+	let posts = [
+		{title: "Post 1", author: "Suzy"},
+		{title: "Post 2", author: "Rick"},
+		{title: "Post 3", author: "Pamela"}
+	];
+
+	res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(port, () => console.log("Server has started"));
